c87ca78eb3fdfa70d4aa2f7815ed931b2b3e5ef3
Will Newton <will.newton@cocoon.life>
2016-03-18 15:28:50 +0000

    Improve test coverage

--- alsa_test.go
+++ alsa_test.go
@@ -19,6 +19,11 @@ func TestCapture(t *testing.T) {
 	a.Equal(c, (*CaptureDevice)(nil), "capture device is nil")
 	a.Error(err, "no device error")
 
+	c, err = NewCaptureDevice("null", 1, FormatS32LE, 0, BufferParams{})
+
+	a.Equal(c, (*CaptureDevice)(nil), "capture device is nil")
+	a.Error(err, "bad rate error")
+
 	c, err = NewCaptureDevice("null", 1, FormatS32LE, 44100, BufferParams{})
 
 	a.NoError(err, "created capture device")
@@ -29,8 +34,20 @@ func TestCapture(t *testing.T) {
 	a.Error(err, "wrong type error")
 	a.Equal(samples, 0, "no samples read")
 
-	b2 := make([]int32, 200)
+	b2 := make([]int16, 200)
 	samples, err = c.Read(b2)
+
+	a.Error(err, "wrong type error")
+	a.Equal(samples, 0, "no samples read")
+
+	b3 := make([]float64, 50)
+	samples, err = c.Read(b3)
+
+	a.Error(err, "wrong type error")
+	a.Equal(samples, 0, "no samples read")
+
+	b4 := make([]int32, 200)
+	samples, err = c.Read(b4)
 
 	a.NoError(err, "read samples ok")
 	a.Equal(len(b2), samples, "correct number of samples read")
@@ -47,6 +64,12 @@ func TestPlayback(t *testing.T) {
 	a.Equal(p, (*PlaybackDevice)(nil), "playback device is nil")
 	a.Error(err, "no device error")
 
+	p, err = NewPlaybackDevice("null", 0, FormatS32LE, 44100,
+		BufferParams{})
+
+	a.Equal(p, (*PlaybackDevice)(nil), "playback device is nil")
+	a.Error(err, "bad channels error")
+
 	p, err = NewPlaybackDevice("null", 1, FormatS32LE, 44100,
 		BufferParams{})
 
@@ -58,8 +81,20 @@ func TestPlayback(t *testing.T) {
 	a.Error(err, "wrong type error")
 	a.Equal(frames, 0, "no frames written")
 
-	b2 := make([]int32, 100)
+	b2 := make([]int16, 100)
 	frames, err = p.Write(b2)
+
+	a.Error(err, "wrong type error")
+	a.Equal(frames, 0, "no frames written")
+
+	b3 := make([]float64, 100)
+	frames, err = p.Write(b3)
+
+	a.Error(err, "wrong type error")
+	a.Equal(frames, 0, "no frames written")
+
+	b4 := make([]int32, 100)
+	frames, err = p.Write(b4)
 
 	a.NoError(err, "buffer written ok")
 	a.Equal(frames, 100, "100 frames written")
